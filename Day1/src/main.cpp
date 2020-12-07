9669c6729ca90059e79575ad3386a17dbdfb2915
Filipe ANTUNES DA SILVA <fantunes.dev@gmail.com>
2020-12-06 23:05:28 -0500

    Part 2 + reformat + simplification

--- Day1/src/main.cpp
+++ Day1/src/main.cpp
@@ -1,41 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <MyMath.h>
 
 #define TARGET_NUMBER 2020
 
-int main()
+void ReadFileToVectorOfUint(const std::string& p_filename, std::vector<uint16_t>& p_entries)
+{
+    std::ifstream file(p_filename);
+    uint16_t dynamicNumber;
+    while (file >> dynamicNumber)
+        p_entries.push_back(dynamicNumber);
+}
+
+int ResolveCalculations(const std::string& p_filename)
 {
     std::vector<uint16_t> entries;
-    std::ifstream file("Expense Report.txt");
-    uint16_t dynamicNumber;
     std::pair<uint16_t, uint16_t> pair;
     uint16_t triple[3]{ 0, 0, 0 };
-    bool pairFound = false;
 
-    while (file >> dynamicNumber)
-    {
-        entries.push_back(dynamicNumber);
-        if (MyMath::Add(entries[0], dynamicNumber) == TARGET_NUMBER)
-        {
-            pair = std::make_pair(entries[0], dynamicNumber);
-            break;
-        }
-    }
+    bool tripleFound = false;
+
+    ReadFileToVectorOfUint(p_filename, entries);
+
     for (uint16_t i = 0; i < entries.size(); ++i)
     {
         for (uint16_t j = 0; j < entries.size(); ++j)
         {
-            if (MyMath::Add(entries[i], entries[j]) == 2020)
+            for (uint16_t k = 0; k < entries.size(); ++k)
+            {
+                if (!tripleFound && (entries[i] + entries[j] + entries[k]) == 2020)
+                {
+                    triple[0] = entries[i];
+                    triple[1] = entries[j];
+                    triple[2] = entries[k];
+                    std::cout << "Triple is : " << triple[0] << " and " << triple[1] << " and " << triple[2] << '\n';
+                    int product = triple[0] * triple[1] * triple[2];
+                    std::cout << "Triple multiplied returns " << product << '\n';
+                    tripleFound = true;
+                }
+            }
+
+            if ((entries[i] + entries[j]) == 2020)
             {
                 pair = std::make_pair(entries[i], entries[j]);
                 std::cout << "Pair is : " << pair.first << " and " << pair.second << '\n';
-                int mult = MyMath::Multiply(pair.first, pair.second);
-                std::cout << "Pair multiplied returns " << mult << '\n';
-                return EXIT_SUCCESS;
+                int product = pair.first * pair.second;
+                std::cout << "Pair multiplied returns " << product << '\n';
             }
+
+            if (pair.first != 0 && triple[0] != 0)
+                return EXIT_SUCCESS;
         }
     }
+    std::cerr << "Error : Something went wrong. Either your file was badly inserted or there were no compatible numbers inside of it.\n";
+    std::cout << "You were looking for " << TARGET_NUMBER << '\n';
     return EXIT_FAILURE;
+}
+
+int main()
+{
+    return ResolveCalculations("Expense Report.txt");
 }
